2452a93674d8e2281d41a4e3c7504c873fe60e42
Paris2020 <nelly.moseki@amazeelabs.com>
2023-11-29 13:47:55 +0200

    Feature - Updated App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+    import HeaderSection from "./components/Header.vue";
+    import FooterSection from "./components/Footer.vue";
+    import IconSymbols from "./components/global/IconSymbols.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="container">
+        <header-section />
+        <footer-section />
+        <icon-symbols />
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+    @import "./assets/scss/style.scss"
 </style>
