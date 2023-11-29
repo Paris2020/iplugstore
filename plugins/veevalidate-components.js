1064635c68af369b8b57ad15a4045f026c99d90c
Paris2020 <nelly.moseki@amazeelabs.com>
2023-11-29 23:01:12 +0200

    Feature - Added vee-validate and yup modules. Created new plugins

---
+++ plugins/veevalidate-components.js
@@ -0,0 +1,10 @@
+
+import { Form, Field, ErrorMessage } from "vee-validate";
+
+export default {
+    install: (app, options) => {
+        app.component("VForm", Form)
+        app.component("VField", Field)
+        app.component("VErrorMessage", ErrorMessage)
+    }
+}
