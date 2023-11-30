32ebcda47c80fcea17f8e279fbc5c90d5e13a172
Paris2020 <nelly.moseki@amazeelabs.com>
2023-11-30 15:39:01 +0200

    Feature - Updated App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
     import HeaderSection from "./components/Header.vue";
     import ContactSection from "./components/Contact.vue";
+    import AboutSection from "./components/About.vue";
     import FooterSection from "./components/Footer.vue";
     import IconSymbols from "./components/global/IconSymbols.vue";
 </script>
@@ -8,6 +9,7 @@
 <template>
     <div class="container">
         <header-section />
+        <about-section />
         <contact-section />
         <footer-section />
         <icon-symbols />
