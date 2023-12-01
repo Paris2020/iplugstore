e6c3c62afbc49929e538b9ea5fcaf0b7e2b0f0a7
Paris2020 <nelly.moseki@amazeelabs.com>
2023-12-01 13:00:54 +0200

    Feature - Updated App.vue file

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
     import HeaderSection from "./components/Header.vue";
     import ContactSection from "./components/Contact.vue";
     import AboutSection from "./components/About.vue";
+    import CatalogueSection from "./components/Catalogue.vue";
+    import ClientSection from "./components/Clients.vue";
     import FooterSection from "./components/Footer.vue";
     import IconSymbols from "./components/global/IconSymbols.vue";
 </script>
@@ -10,6 +12,8 @@
     <div class="container">
         <header-section />
         <about-section />
+        <catalogue-section />
+        <client-section />
         <contact-section />
         <footer-section />
         <icon-symbols />
