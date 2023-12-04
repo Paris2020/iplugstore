7b42b95da9c42ce4b937e83b713f2c4e13c02071
Paris2020 <nelly.moseki@amazeelabs.com>
2023-12-04 12:35:51 +0200

    Feature - Add AnimatedCounter module in main.js file

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 
 import VeeValidateComponents from "../plugins/veevalidate-components"
 import VeeValidateRules from "../plugins/veevalidate-rules"
+import AnimatedCounter from "vue-animated-counter"
 
 const app = createApp(App);
 
 app.use(VeeValidateComponents)
 app.use(VeeValidateRules)
+app.component("AnimatedCounter", AnimatedCounter)
 
 app.mount("#app");
 //createApp(App).mount('#app')
